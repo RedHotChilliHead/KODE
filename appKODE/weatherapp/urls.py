065ce43fa09197274ad5977ac38b2c134ff82e60
Red <irina.purgina@inbox.ru>
2024-07-28 19:34:50 +0300

    new weatherapp

---
+++ appKODE/weatherapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+
+app_name = "weatherapp"
+
+# router = DefaultRouter()
+# router.register(r'suppliers', SupplierViewSet)
+#
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
